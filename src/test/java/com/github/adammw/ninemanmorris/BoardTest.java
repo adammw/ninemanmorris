0da5ba8528775228be21352d7a8ee932960b41f2
Adam Malcontenti-Wilson <amalcontenti-wilson@zendesk.com>
2016-05-17 22:44:44 +1000

    Add more Board class tests

--- src/test/java/com/github/adammw/ninemanmorris/BoardTest.java
+++ src/test/java/com/github/adammw/ninemanmorris/BoardTest.java
@@ -21,6 +21,7 @@ public class BoardTest {
     private Board board;
     private Piece[][] internalBoard;
     private HashMap<Player, List<Piece>> playerPieces;
+    private Board.MillFormedCallback callback;
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
@@ -28,6 +29,7 @@ public class BoardTest {
     @Before
     public void setUp() throws Exception {
         controller = mock(GameController.class);
+        callback = mock(Board.MillFormedCallback.class);
         players = new Player[] {
                 PlayerFactory.build(controller, PlayerType.HUMAN_PLAYER),
                 PlayerFactory.build(controller, PlayerType.HUMAN_PLAYER)
@@ -60,9 +62,9 @@ public class BoardTest {
     @Test
     public void testPlacingPiece() throws Exception {
         Move move = new Move(null, "a1");
-        board.performMove(move, players[0], () -> {
-            fail("Mill should not have been formed");
-        });
+        board.performMove(move, players[0], callback);
+        verifyZeroInteractions(callback);
+        assertNotNull(internalBoard[0][0]);
     }
 
     @Test
@@ -73,21 +75,166 @@ public class BoardTest {
         thrown.expectMessage("Board location is occupied");
 
         Move move = new Move(null, "a1");
-        board.performMove(move, players[0], () -> {
-            fail("Mill should not have been formed");
-        });
+        board.performMove(move, players[0], callback);
+        verifyZeroInteractions(callback);
     }
 
 
     @Test
-    public void testMillDetection() throws Exception {
+    public void testMillDetectionColumn() throws Exception {
         internalBoard[2][3] = playerPieces.get(players[0]).remove(0);
         internalBoard[1][3] = playerPieces.get(players[0]).remove(0);
 
-        boolean millFormed = false;
         Move move = new Move(null, "d1");
-        Board.MillFormedCallback callback = mock(Board.MillFormedCallback.class);
         board.performMove(move, players[0], callback);
         verify(callback).millFormed();
     }
+
+    @Test
+    public void testMillDetectionRow() throws Exception {
+        internalBoard[6][0] = playerPieces.get(players[0]).remove(0);
+        internalBoard[6][6] = playerPieces.get(players[0]).remove(0);
+
+        Move move = new Move(null, "d7");
+        board.performMove(move, players[0], callback);
+        verify(callback).millFormed();
+    }
+
+    @Test
+    public void testOpponentMillNotDetected() throws Exception {
+        internalBoard[0][0] = playerPieces.get(players[0]).remove(0);
+        internalBoard[0][3] = playerPieces.get(players[0]).remove(0);
+
+        Move move = new Move(null, "g1");
+        board.performMove(move, players[1], callback);
+        verifyZeroInteractions(callback);
+    }
+
+    @Test
+    public void testAcrossMidpointMillNotDetected() throws Exception {
+        internalBoard[3][1] = playerPieces.get(players[0]).remove(0);
+        internalBoard[3][2] = playerPieces.get(players[0]).remove(0);
+        internalBoard[1][3] = playerPieces.get(players[0]).remove(0);
+        internalBoard[2][3] = playerPieces.get(players[0]).remove(0);
+
+        board.performMove(new Move(null, "e4"), players[0], callback);
+        verifyZeroInteractions(callback);
+
+        board.performMove(new Move(null, "d5"), players[0], callback);
+        verifyZeroInteractions(callback);
+    }
+
+    @Test
+    public void testCantRemoveNoPiece() throws Exception {
+        internalBoard[6][0] = playerPieces.get(players[0]).remove(0);
+        internalBoard[6][6] = playerPieces.get(players[0]).remove(0);
+
+        Move move = new Move(null, "d1");
+        board.performMove(move, players[0], callback);
+
+        thrown.expect(Board.IllegalMoveException.class);
+        thrown.expectMessage("There is no piece at the specified location");
+
+        board.performMove(new Move("d7", null), players[0], null);
+    }
+
+    @Test
+    public void testCantRemoveOwnPiece() throws Exception {
+        internalBoard[6][0] = playerPieces.get(players[0]).remove(0);
+        internalBoard[6][6] = playerPieces.get(players[0]).remove(0);
+
+        Move move = new Move(null, "d7");
+        board.performMove(move, players[0], callback);
+
+        thrown.expect(Board.IllegalMoveException.class);
+        thrown.expectMessage("Can't remove your own piece");
+
+        board.performMove(new Move("d7", null), players[0], null);
+    }
+
+
+    @Test
+    public void testCantRemoveMillPieceUnlessNoRemaining() throws Exception {
+        final Move removeA1 = new Move("a1", null);
+        final Move removeD2 = new Move("d2", null);
+
+        // Set up the board with the opponent having a mill a1 - g1, and also a piece in d2
+        internalBoard[0][0] = playerPieces.get(players[1]).remove(0);
+        internalBoard[0][3] = playerPieces.get(players[1]).remove(0);
+        internalBoard[0][6] = playerPieces.get(players[1]).remove(0);
+        internalBoard[1][3] = playerPieces.get(players[1]).remove(0);
+
+        // Create a mill a7-g7
+        internalBoard[6][0] = playerPieces.get(players[0]).remove(0);
+        internalBoard[6][6] = playerPieces.get(players[0]).remove(0);
+        board.performMove(new Move(null, "d7"), players[0], () -> {
+            // Expect removal of a1 to fail (non-mill pieces exist)
+            try {
+                board.performMove(removeA1, players[0], null);
+                fail();
+            } catch(Board.IllegalMoveException expectedException) {
+                assertEquals(expectedException.getMessage(), "Can't remove a piece which is part of a mill");
+            }
+
+            // Expect removal of d2 to succeed
+            try {
+                board.performMove(removeD2, players[0], null);
+            } catch(Board.IllegalMoveException exception) {
+                fail(exception.getMessage());
+            }
+            assertNull(internalBoard[1][3]);
+        });
+
+
+        // Now only a mill remains, expect removal of a1 to succeed
+        internalBoard[6][3] = null;
+        board.performMove(new Move(null, "d7"), players[0], () -> {
+            try {
+                board.performMove(removeA1, players[0], null);
+            } catch(Board.IllegalMoveException exception) {
+                fail(exception.getMessage());
+            }
+            assertNull(internalBoard[0][0]);
+        });
+    }
+
+    @Test
+    public void testGameStageTransitions() throws Exception {
+        for (int i = 0; i < 5; i++) { playerPieces.get(players[0]).remove(0); } // throw 5 pieces away and place 4
+        board.performMove(new Move(null, "a1"), players[0], callback);
+        board.performMove(new Move(null, "d2"), players[0], callback);
+        board.performMove(new Move(null, "g4"), players[0], callback);
+        board.performMove(new Move(null, "a4"), players[0], callback);
+
+        assertEquals(board.getStage(players[0]), GameStage.MOVING);
+
+        // form a mill to remove one of player 1's pieces
+        internalBoard[6][0] = playerPieces.get(players[1]).remove(0);
+        internalBoard[6][3] = playerPieces.get(players[1]).remove(0);
+        Move removeA4 = new Move("a4", null);
+        board.performMove(new Move(null, "g7"), players[1], () -> {
+            try {
+                board.performMove(removeA4, players[1], null);
+            } catch (Board.IllegalMoveException e) {
+                fail(e.getMessage());
+            }
+        });
+
+        assertEquals(board.getStage(players[0]), GameStage.FLYING);
+
+        // form a mill to remove one of player 1's pieces
+        internalBoard[6][6] = null;
+        Move removeA1 = new Move("a1", null);
+        board.performMove(new Move(null, "g7"), players[1], () -> {
+            try {
+                board.performMove(removeA1, players[1], null);
+            } catch (Board.IllegalMoveException e) {
+                fail(e.getMessage());
+            }
+        });
+
+        assertEquals(board.getStage(players[0]), GameStage.GAME_OVER);
+        assertTrue(board.isGameOver());
+    }
+
 }
